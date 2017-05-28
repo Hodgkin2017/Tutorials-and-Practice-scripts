8345927d4bb565e8327c51e7908a15c0dee7c672
Hodgkin2017 <mcb53@cam.ac.uk>
2017-05-28 17:02:39 +0100

    Testing parallelisation/multiple cores

---
+++ Multiple cores R.R
@@ -0,0 +1,271 @@
+###################
+### Using multiple cores with R
+###################
+
+##See website: http://gforge.se/2015/02/how-to-go-parallel-in-r-basics-tips/
+
+library(parallel)
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+no_cores
+
+# Initiate cluster
+cl <- makeCluster(no_cores)
+cl
+
+lapply(1e6:2.5e7,function(exponent) 2^exponent)
+
+
+parLapply(cl, 1e6:2.5e7,
+          function(exponent)
+            2^exponent
+)
+
+stopCluster(cl)
+
+#########
+##Include objects in cluster:
+cl<-makeCluster(no_cores)
+##base not part of environment:
+base <- 2
+
+parLapply(cl, 
+          2:4, 
+          function(exponent) 
+            base^exponent)
+
+stopCluster(cl)
+
+##base part of environment
+cl<-makeCluster(no_cores)
+
+base <- 2
+clusterExport(cl, "base")
+parLapply(cl, 
+          2:4, 
+          function(exponent) 
+            base^exponent)
+
+stopCluster(cl)
+
+##Import R library to parLapply:
+
+clusterEvalQ(cl, library(rms))
+
+###########
+##parSapply
+
+cl <- makeCluster(no_cores)
+cl
+
+parSapply(cl, 2:4,
+          function(exponent)
+            2^exponent
+)
+
+stopCluster(cl)
+
+#Matrix output with names (this is why we need the as.character):
+
+cl <- makeCluster(no_cores)
+cl
+clusterExport(cl, "base")
+parSapply(cl, as.character(2:4), 
+          function(exponent){
+            x <- as.numeric(exponent)
+            c(base = base^x, self = x^x)
+          })
+
+stopCluster(cl)
+
+##########
+#foreach package
+
+library(foreach)
+library(doParallel)
+
+cl<-makeCluster(no_cores)
+registerDoParallel(cl)
+
+#If use: registerDoParallel(no_cores)
+#the use: stopImplicitCluster()
+#clusterExport(cl, "base")
+#stopCluster()
+
+foreach(exponent = 2:4, 
+        .combine = c)  %dopar%  
+  base^exponent
+
+foreach(exponent = 2:4, 
+        .combine = rbind)  %dopar%  
+  base^exponent
+
+foreach(exponent = 2:4, 
+        .combine = list,
+        .multicombine = TRUE)  %dopar%  
+  base^exponent
+
+foreach(exponent = 2:4, 
+        .combine = list)  %dopar%  
+  base^exponent
+
+#######
+#Local environment variables available:
+  
+base <- 2
+cl<-makeCluster(2)
+cl
+registerDoParallel(cl)
+foreach(exponent = 2:4, 
+        .combine = c)  %dopar%  
+  base^exponent
+stopCluster(cl)
+
+##does not work as base not available in local environment:
+test <- function (exponent) {
+  foreach(exponent = 2:4, 
+          .combine = c)  %dopar%  
+    base^exponent
+}
+test()
+
+##.export loads local variables:
+base <- 2
+cl<-makeCluster(2)
+registerDoParallel(cl)
+
+base <- 4
+test <- function (exponent) {
+  foreach(exponent = 2:4, 
+          .combine = c,
+          .export = "base")  %dopar%  
+    base^exponent
+}
+test()
+
+stopCluster(cl)
+
+##For exporting packages into foreach:
+.packages = c("rms", "mice")
+
+###########
+##foreach speed test:
+
+system.time(lapply(c(1e100000, 1e100000, 1e100000, 1e100000, 1e100000, 1e100000, 1e100000),function(exponent) 
+  sd(sd(sd(log(mean(log(log((log(log(1e100^exponent)/exponent*36)/exponent))))))))))
+
+
+cl<-makeCluster(7)
+registerDoParallel(cl)
+
+base <- 2
+test <- function (exponent) {
+  foreach(exponent = c(1e100000, 1e100000, 1e100000, 1e100000, 1e100000, 1e100000, 1e100000),
+                  .combine = list,
+                  .multicombine = TRUE,
+          .export = "base")  %dopar%  
+    sd(sd(sd(log(mean(log(log((log(log(1e100^exponent)/exponent*36)/exponent))))))))
+}
+system.time(test())
+
+stopCluster(cl)
+
+#foreach = very slow!!!!
+
+#############
+##FORK is more memory efficient
+cl<-makeCluster(no_cores, type="FORK")
+
+############
+##Another option
+##This one seems to work!!!!!!!!
+
+system.time(test1<- lapply(1:1000, function(i) sqrt(1/(sin(i))^2)-sum(rnorm(10^6))))
+
+install.packages("doMC")
+
+library(doMC)
+options(cores = 7)
+registerDoMC()
+
+my.function<- function(){
+  data=vector(length=1000)
+data=foreach(i=1:1000) %dopar% {
+  sqrt(1/(sin(i))^2)-sum(rnorm(10^6))
+}
+}
+system.time(test<- my.function())
+test
+test=unlist(test)
+rm(test)
+
+##############
+## Look up mclapply....
+
+#https://www.r-bloggers.com/a-no-bs-guide-to-the-basics-of-parallelization-in-r/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################
+#From Henrik: Sample code looks like this:
+library(parallel)
+rm(list=ls()) # Remove old environment
+cl = makeCluster(detectCores()-1) # Set number of cores
+clusterEvalQ(cl, require(repsych))
+clusterExport(cl, as.list(ls())) # Export all variables
+
+# Actual usage
+results <- parLapply(cl, input, function(ii, ...) your.function(ii))
+stopCluster(cl) # Kill the cluster
+
+
+
+
+##Parrallising R using bash:
+
+#!/bin/sh
+
+## This script runs the simulations in R in parallel
+
+# run () {
+#   START=$1
+#   END=$2
+#   for ii in $(seq $START $END); do
+#   ( 
+#     RETURN_VAL=$(nice -n 5 Rscript --vanilla /local/data/public/hpa22/assignments/syba1/gillespie.R
+#     ) &
+#       done
+# }
+# 
+# run 1 20
+# wait
+# run 21 40
+# wait
+# run 41 60
+# wait
+# run 61 80
+# wait
+# run 81 100
+# 
+# exit
+
+##########
+
+#On Linux there is also the GNU parallel command, which is super efficient, but somewhat tricky to use.
+#The best reference is probably the official:
+#  https://www.gnu.org/software/bash/manual/html_node/GNU-Parallel.html
--- apply function test.R
+++ apply function test.R
@@ -31,3 +31,283 @@ apply(m,2,function(x) mean(x))
 sapply(1:3, function(x, y) mean(y[,x]), y=m)
 
 
+###############
+
+################
+##Data
+
+CNV.data<-cnv.list
+
+CNV.data[[38]]<- CNV.all.table
+
+##############
+###Learning
+lapply(1:3, function(x) c(x, x^2, x^3))
+
+new.function<- function(x){
+  (x+1)*2
+}
+
+lapply(1:3, function(x) new.function(x))
+
+x<- matrix(rpois(1e4, 8), 1e3)
+dim(x)
+
+apply(x, 2, mean)
+apply(x, 2, function(x) sd(x)/sqrt(length(x)))
+apply(x, 2, function(x) {sd(x)/sqrt(length(x))})
+
+GETCI<- function(x, level=0.95){
+  if(level <= 0 || level >=1) {
+    stop("The 'level' argument must be >- and <1")
+  }
+  if (level < 0.5) {
+    warning("Confidence levels are often close to 1, e.g. 0.95")
+  }
+  
+  m <-mean(x)
+  n<- length(x)
+  SE<- sd(x)
+  upper<- 1 - (1-level)/2
+  ci<- m+c(-1,1)*qt(upper, n-1)*SE
+  return(list(mean=m, se=SE, ci=ci))
+  
+}
+
+apply(x, 2, function(x) GETCI(x))
+
+
+#####################
+### Learning how to use lappy with my own function to speak up data processing(hopefully!)
+
+
+### Function to process data
+
+length(CNV.data)
+
+
+
+new.function<- function(x,y){
+  
+  z<- dplyr::full_join(y[,1:8],x, by = "Locus.ID")
+  cytoband.list<- events.per.cytoband(z, threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE)
+  return(cytoband.list[[2]]$proportion.of.deletions)
+  
+}
+
+system.time(test5<- lapply(CNV.data, function(x) {new.function(x, acc.cnv.chr.location)}))
+
+# ?do.call
+# length(test5)
+# str(test5)
+test6<- do.call(cbind, test5) %>%as.matrix
+# class(test6)
+# dim(test6)
+# all.equal(test6, heatmap.matrix.cytoband.del)
+# identical(test6, heatmap.matrix.cytoband.del)
+# head(heatmap.matrix.cytoband.del)
+# class(heatmap.matrix.cytoband.del)
+
+
+rownames(test6)<- cytoband.list[[2]]$cytoband
+colnames(test6)<- c(cancer.type, "ALL")
+
+
+
+pheatmap(test6,
+         cluster_row = F,
+         cluster_cols = F,
+         show_rownames = TRUE,
+         show_colnames = TRUE,
+         color = col.pal,
+         fontsize_row=1,
+         #cellwidth = 10,
+         annotation_row = annotation_row,
+         annotation_legend = FALSE
+)
+
+
+##########
+## Try multiple cores:
+
+library(doMC)
+options(cores = 7)
+registerDoMC()
+
+CNV.data<-cnv.list
+
+CNV.data[[38]]<- CNV.all.table
+
+
+new.function<- function(x,y){
+  
+  z<- dplyr::full_join(y[,1:8],x, by = "Locus.ID")
+  cytoband.list<- events.per.cytoband(z, threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE)
+  return(cytoband.list[[2]]$proportion.of.deletions)
+  
+}
+
+system.time(test5<- lapply(CNV.data, function(x) {new.function(x, acc.cnv.chr.location)}))
+
+
+##########
+##multi-core test 1:
+
+output=vector(length=38)
+output
+
+output=foreach(i = CNV.data) %dopar% {
+  i[,1:3]
+}
+
+length(output)
+class(output)
+dim(output[[1]])
+
+##########
+##multi-core test 2:
+
+## Get chromosomal locations:
+acc.cnv<-chromosomal_location(cnv.list[[1]])
+head(acc.cnv[,1:12])
+
+##multi-core loop:
+
+output=vector(length=38)
+#works:
+output=foreach(i = CNV.data) %dopar% {
+  acc.cnv[,1:8]}
+output[[1]]
+#works:
+output=vector(length=38)
+output=foreach(i = CNV.data) %dopar% {
+  dplyr::full_join(acc.cnv[,1:8],acc.cnv[,1:3], by = "Locus.ID")}
+output[[1]]
+#does not work...error:
+output=vector(length=38)
+output=foreach(i = CNV.data) %dopar% {
+  dplyr::full_join(acc.cnv[,1:8],i, by = "Locus.ID")}
+output[[1]]
+
+#does not work...error:
+output=vector(length=38)
+output=foreach(i = 1:38) %dopar% {
+  dplyr::full_join(acc.cnv[,1:8],CNV.data[[i]], by = "Locus.ID")}
+output[[1]]
+
+#does not work...error:
+output=vector(length=38)
+output=foreach(i=CNV.data) %dopar% {
+  i %>% dplyr::full_join(acc.cnv[,1:8],., by = "Locus.ID")}
+output[[1]]
+
+  ############
+##rest of function:
+  cytoband.list<- events.per.cytoband(z, threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE)
+  return(cytoband.list[[2]]$proportion.of.deletions)
+  }
+
+output=foreach(i = CNV.data) %dopar% {
+  dplyr::full_join(acc.cnv[,1:8],i, by = "Locus.ID") }
+#%>%
+  #events.per.cytoband(., threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE %>%
+  #return(.[[2]]$proportion.of.deletions)
+#}
+
+length(output)
+head(output[[1]][,1:12])
+
+############
+##multi-core test 3:
+
+new.function2<- function(x,y){
+  
+  x %>% dplyr::full_join(y[,1:8],., by = "Locus.ID") %>%
+  events.per.cytoband(., threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE) %>%
+  return(.[[2]]$proportion.of.deletions)
+  
+}
+
+
+library(parallel)
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+no_cores
+no_cores<- 4
+
+# Initiate cluster
+cl <- makeCluster(no_cores)
+cl
+
+clusterExport(cl, "acc.cnv")
+clusterExport(cl, "new.function2")
+clusterExport(cl, "events.per.cytoband")
+clusterEvalQ(cl, library(dplyr))
+
+test5=vector(length=38)
+system.time(test5<- parLapply(cl, CNV.data, function(x) {new.function2(x, acc.cnv)}))
+
+stopCluster(cl)
+
+length(test5)
+head(test5[[1]])
+##Time = 157 (50s faster....not much better)
+
+test6<- do.call(cbind, test5) %>%as.matrix
+
+
+
+rownames(test6)<- CNV.data[[1]]$cytoband
+colnames(test6)<- c(cancer.type, "ALL")
+
+
+
+pheatmap(test6,
+         cluster_row = F,
+         cluster_cols = F,
+         show_rownames = TRUE,
+         show_colnames = TRUE,
+         color = col.pal,
+         fontsize_row=1
+         #cellwidth = 10,
+         #annotation_row = annotation_row,
+         #annotation_legend = FALSE
+)
+
+
+
+##############
+##Old method:
+
+#old.function<- function(cnv.list, acc.cnv.chr.location, CNV.all.table){}
+old.function<- function(){
+  cancer.type<- names(cnv.list)
+  cancer.type
+  
+  heatmap.matrix.cytoband.del<- matrix(NA, ncol = length(cancer.type)+1, nrow = 806)
+  
+  
+  for (i in 1:length(cancer.type)){
+    
+    x<-cnv.list[[i]]
+    x<- dplyr::full_join(acc.cnv.chr.location[,1:8],x, by = "Locus.ID")
+    cytoband.list<- events.per.cytoband(x, threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE)
+    heatmap.matrix.cytoband.del[,i]<- cytoband.list[[2]]$proportion.of.deletions
+    #print(cancer.type[i])
+  }
+  
+  #head(heatmap.matrix.cytoband.del)
+  
+  ##Final column contains all cancer types:
+  
+  ##Create one large dataframe with all CNV data in it:
+  #x<-join.cnv.datasets(cnv.list, 4)
+  
+  ##Calculate proportion of deletions per cytoband and add to matrix
+  x<- dplyr::full_join(acc.cnv.chr.location[,1:8],CNV.all.table, by = "Locus.ID")
+  cytoband.list<- events.per.cytoband(x, threshold = -1, cytoband_column = 10, column_data_start = 11, chromosome_interval = 0,  deletion = TRUE)
+  heatmap.matrix.cytoband.del[,38]<- cytoband.list[[2]]$proportion.of.deletions
+}
+
+system.time(old.function())
